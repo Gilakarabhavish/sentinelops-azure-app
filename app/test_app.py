a0b760861cd8064d0005c4a4899a284b126fa9a1
bob <bhavishgilakara@gmail.com>
2026-07-14 11:29:29 +0530

    it will work now!

--- app/test_app.py
+++ app/test_app.py
@@ -1,16 +1,22 @@
-import json
 from app import application
 
 
 def test_home():
     client = application.test_client()
+
     response = client.get("/")
+
     assert response.status_code == 200
-    data = json.loads(response.data)
-    assert data["status"] == "running"
+    assert b"SentinelOps Lite" in response.data
 
 
 def test_health():
     client = application.test_client()
+
     response = client.get("/health")
+
     assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["status"] == "healthy"
